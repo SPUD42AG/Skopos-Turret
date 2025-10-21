1ad90ada7d689bac75e148beff403f1c090e6db5
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-10-20 22:15:10 -0700

    Create mode switch interface and remove more template code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.SlotConfigs;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +17,44 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    
+    public static final class ShooterConstants {
+    
+        public static final int SHOOTER_MOTOR_ID = 0;
+        
+        public static final double P = 0;
+        public static final double I = 0;
+        public static final double D = 0;
+    
+        public static final SlotConfigs PIDConfigs = new SlotConfigs()
+            .withKP(P)
+            .withKI(I)
+            .withKD(D);
+        
+        public static final boolean SUPPLY_CURRENT_LIMIT_ENABLE = true;
+        public static final double SUPPLY_CURRENT_LIMIT = 0;
+        public static final double SUPPLY_CURRENT_LOWER_LIMIT = 0;
+        public static final double SUPPLY_CURRENT_LOWER_TIME = 1;
+
+        public static final CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs()
+            .withSupplyCurrentLimitEnable(SUPPLY_CURRENT_LIMIT_ENABLE)
+            .withSupplyCurrentLimit(SUPPLY_CURRENT_LIMIT)
+            .withSupplyCurrentLowerLimit(SUPPLY_CURRENT_LOWER_LIMIT)
+            .withSupplyCurrentLowerTime(SUPPLY_CURRENT_LOWER_LIMIT);
+    
+        public static final double SENSOR_MECHANISM_RATIO = 0;
+
+        public static final FeedbackConfigs feedbackConfig = new FeedbackConfigs()
+            .withSensorToMechanismRatio(SENSOR_MECHANISM_RATIO);
+        
+            public enum ShooterState {
+            PLACE_HOLDER(0);
+            
+            public final double speed;
+        
+            private ShooterState(double speed) {
+                this.speed = speed;
+            }
+        }
+    }
 }
