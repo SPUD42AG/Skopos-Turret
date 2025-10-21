816f48f2eb734a8895d8f42b16f63e8073cf7b96
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-10-20 22:15:46 -0700

    Create ShooterSubsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.superstructure.HoodSubsystem;
 import frc.robot.subsystems.superstructure.IntakeSubsystem;
 import frc.robot.subsystems.superstructure.ShooterSubsystem;
@@ -27,8 +26,6 @@ public class RobotContainer {
   private final TurretSubsystem turretSubsystem = new TurretSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/subsystems/superstructure/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/superstructure/ShooterSubsystem.java
@@ -1,5 +1,72 @@
 package frc.robot.subsystems.superstructure;
 
-public class ShooterSubsystem {
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.ShooterConstants.*;
+import frc.robot.Constants.ShooterConstants.ShooterState;
+import frc.robot.utilities.ModeSwitchHandler.ModeSwitchInterface;
+
+public class ShooterSubsystem extends SubsystemBase implements ModeSwitchInterface{
     
+    private TalonFX motor;
+
+    private VelocityVoltage velocityVoltage = new VelocityVoltage(0);
+
+    public double setpoint = 0.0;
+
+
+    public ShooterSubsystem() {
+        initializeMotors();
+    }
+
+    //#region Initzalization
+
+    private void initializeMotors() {
+        motor = new TalonFX(SHOOTER_MOTOR_ID);
+            motor.setNeutralMode(NeutralModeValue.Brake);
+
+        var mConfigurator = motor.getConfigurator();
+            mConfigurator.apply(PIDConfigs);
+            mConfigurator.apply(currentLimits);
+            mConfigurator.apply(feedbackConfig);
+    }
+
+    //#endregion
+    //#region Measurment
+
+    public AngularVelocity getSpeed(){
+        return RPM.of(motor.getVelocity().getValue().in(RPM));
+    }
+
+    //#endregion
+    //#region Movement
+
+    private void setSpeed(double speed) {
+        velocityVoltage.Velocity = speed;
+        motor.setControl(velocityVoltage);
+        setpoint = speed;
+    }
+
+    public void setPresetSpeed (ShooterState speed) {
+        setSpeed(speed.speed);
+    }
+
+    //#endregion
+    //#region Commands
+
+    public Command setSpeedCommand(double newSpeed){
+        return Commands.runOnce(() -> setSpeed(newSpeed));
+    }
+
+    @Override
+    public void onModeSwitch() {
+        setPresetSpeed(ShooterState.PLACE_HOLDER);
+    }
 }
